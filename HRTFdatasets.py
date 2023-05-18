1a68dad959473d5ea2e69a1a635659325b82f3a4
YutongWen <ywen6@u.rochester.edu>
2023-05-18 14:57:28 -0400

    add how to train as comment

--- HRTFdatasets.py
+++ HRTFdatasets.py
@@ -11,6 +11,22 @@ import matplotlib
 from scipy.signal import minimum_phase
 from scipy.signal import hilbert
 
+# how to train
+# norm way 4 is proposed
+# norm way 5 is mix_pos_mix_ear
+# norm way 6 is mix_ear_individual_pos
+# norm way 2 is equator
+# please modify the output folder to include the norm way info (-o dir)
+# exp4
+# python3 hrtf_normalization/train.py -o hrtf_normalization/exp4 -n hutubs 3d3a bili listen crossmod cipic ita sadie ari
+# exp1
+# python3 hrtf_normalization/train.py -o hrtf_normalization/exp1 -n listen crossmod sadie bili -t ari --norm_way 2
+# exp2
+# python3 hrtf_normalization/train.py -o hrtf_normalization/exp2 -n ari crossmod hutubs cipic -t riec --norm_way 2
+# exp3
+# python3 hrtf_normalization/train.py -o hrtf_normalization/exp3 -n crossmod sadie bili -t listen --norm_way 2
+# exp5
+# python3 hrtf_normalization/train.py -o hrtf_normalization/exp5 -n listen 3d3a bili crossmod cipic ita sadie ari riec -t hutubs --norm_way 2
 
 
 class HRTFDataset(Dataset):
